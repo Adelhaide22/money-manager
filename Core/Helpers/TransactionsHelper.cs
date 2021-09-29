8762c74ee30b1106220c62b380c4192e9276169b
Alina Siriak <adelhaide22@gmail.com>
2021-09-29 16:10:18 +0300

    Split StateManager into helpers

---
+++ Core/Helpers/TransactionsHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Helpers
+{
+    public static class TransactionsHelper
+    {
+        public static IEnumerable<Transaction> GetTransactionsUnion(IEnumerable<string> categories, Date start, Date end)
+        {
+            Func<Transaction, bool> filter = t => categories.Any(c => CategoriesHelper.GetFilterForCategory(c)(t));
+            return State.Instance.Transactions
+                               .Where(t => t.Date >= start && t.Date <= end)
+                               .Where(filter)
+                               .OrderBy(t => t.Date)
+                               .ToList();
+        }
+    }
+}
